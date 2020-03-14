17de0a42162f204673d1b5e3f729533901cc6f17
Kimberly Wilkes <49125411+kimdolion@users.noreply.github.com>
2020-03-14 19:23:25 -0400

    Update with comments and rearrange code
    - Updated with more comments as per project requirements
    - Moved function leastCommonMultiple to be after greatestCommonDenom
    - Added more echo checks for values: temp1 holds intiial a value in greatestCommonDenom
    
    TODO:
    - Pass LCM an array and have GCD be passed the values in the array.
    - Calculate LCM and GCD of more than 2 variables from an array.

--- lcmEuclidean.cpp
+++ lcmEuclidean.cpp
@@ -1,23 +1,27 @@
+// Kimberly Wilkes
+// CIT 237 - C++
+// Project 1 - Euclidean LCM
+// March 16, 2020
+
 /*
 Write a c++ program to find least common multiple (LCM) of two, three and four integer values.
 The integer values are entered from the keyboard and the outputs are printed to the console.
 The LCM of two, three and four integer values are computed using the Euclidean algorithm.
 You have to use arrays to hold input values and use functions/methods to get data from the keyboard,
-    display output to the console, calculate LCM using Euclidean algorithm.  
+    display output to the console, calculate LCM using Euclidean algorithm.
+    // LCM(x,y)=(x*y)/GCD(x,y) 
 */
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 // LCM(x,y)=(x*y)/GCD(x,y) 
-/*
-int greatestCommonDenom(int, int);
-*/
 void displayArray(int[], int);
+int greatestCommonDenom(int, int);
+int leastCommonMultiple(int[], int);
 
 int main() {
-    int sizeOfArray, userValue;
+    int sizeOfArray, userValue, result;
     cout << "You can enter between 2 and 4 numbers into this LCM calculator.\n"
         << "Enter how many numbers you want to use for this LCM calculator: ";
     cin >> sizeOfArray;
@@ -41,11 +45,13 @@ int main() {
             }
             else {
                 cout << "You must enter values between 2 and 100.\n";
-                i-= 1;
+                i-= 1; // reduce the iterator by 1 because of bad input
             }
         }
-        cout << endl;
+        cout << "\n\nYou entered: ";
         displayArray(values, sizeOfArray);
+        result = leastCommonMultiple(values, sizeOfArray);
+        cout << "Least common multiple of all numbers in the array is: " << result;
         
     }
     return 0;
@@ -57,19 +63,27 @@ void displayArray(int arr[], int size) {
     }
 }
 /*
-int leastCommonMultiple() {
-    // LCM(x,y)=(x*y)/GCD(x,y) 
-    greatestCommonDenom();
-}
-
 int greatestCommonDenom(int a, int b) { // find the greatest common denominator between 2 numbers
-    int remain;
+    int remain, temp1 = a, temp2 = b;
     // find the remainder of two variables
     while ((a % b) > 0)  {
-        remain = a % b;
-        a = b;
-        b = remain;
+        remain = a % b; // find the remainder of these two numbers
+        a = b; // set a to b for the next iteration
+        b = remain; // set b to the remainder for the next iteration until we have a remainder of 0
     }
-  return b;
+    cout << "GCD of " << temp1 << " and " << temp2 " is " << b << endl;
+    return b;
+}
+
+int leastCommonMultiple(int arr[], int size) {
+    // LCM(x,y)=(x*y)/GCD(x,y) equation for calculating the least common multiple using greatest common denominator
+    int gcd, lcm, x, y;
+    for (int i = 0; i < size; i++) {
+        
+    }
+    gcd = greatestCommonDenom(x, y);
+    lcm = (x * y) / gcd;
+    cout << "Least common multiple is: " << lcm;
+    return lcm;
 }
 */
