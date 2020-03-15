87fec0e2de10564dfd0bc3be94f700535fd9d7c9
Kimberly Wilkes <49125411+kimdolion@users.noreply.github.com>
2020-03-14 20:30:27 -0400

    Update displayArray function again
    - Updated displayArray function to have a newline when the for loop is finished to better see following results

--- lcmEuclidean.cpp
+++ lcmEuclidean.cpp
@@ -61,6 +61,7 @@ void displayArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         cout << arr[i] << "  ";
     }
+    cout << endl;
 }
 
 int greatestCommonDenom(int a, int b) { // find the greatest common denominator between 2 numbers
